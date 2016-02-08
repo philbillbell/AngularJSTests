c066fc28e49a3d9baf55726c9f0810bd49765493
philbillbell <philbillbell@hotmail.com>
2016-02-08 14:46:00 +0000

    minor updates to test changes

--- order-form/script.js
+++ order-form/script.js
@@ -1,5 +1,6 @@
-function OrderFormController($scope){
 
+function OrderFormController($scope) {
+    "use strict";
 	// Define the model properties. The view will loop
 	// through the services array and genreate a li
 	// element for every one of its items.
@@ -8,38 +9,42 @@ function OrderFormController($scope){
 		{
 			name: 'Web Development',
 			price: 300,
-			active:true
-		},{
+			active: false
+		}, {
 			name: 'Design',
 			price: 400,
-			active:false
-		},{
+			active: false
+		}, {
 			name: 'Integration',
 			price: 250,
-			active:false
-		},{
+			active: false
+		}, {
 			name: 'Training',
 			price: 220,
-			active:false
+			active: false
+		}, {
+			name: 'Running',
+			price: 150,
+			active: false
 		}
 	];
 
-	$scope.toggleActive = function(s){
+	$scope.toggleActive = function (s) {
 		s.active = !s.active;
 	};
 
 	// Helper method for calculating the total price
 
-	$scope.total = function(){
+	$scope.total = function () {
 
 		var total = 0;
 
 		// Use the angular forEach helper method to
 		// loop through the services array:
 
-		angular.forEach($scope.services, function(s){
-			if (s.active){
-				total+= s.price;
+		angular.forEach($scope.services, function (s) {
+			if (s.active) {
+				total += s.price;
 			}
 		});
 
